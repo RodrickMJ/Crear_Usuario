4ea584feb5b8193c58754960e79f493b992d24d5
KiritoKazut0 <norbertolopez291@gmail.com>
2024-05-24 06:33:49 -0600

    app inicialitation

--- src/Users/infrastructure/UserRouter.ts
+++ src/Users/infrastructure/UserRouter.ts
@@ -1,7 +1,10 @@
 import { Router } from "express";
 import {createUserController, getByIdUser} from "./dependencies"
+import '../../Database/Connection'
 
 const userRouter = Router();
 
 userRouter.post('/', createUserController.run.bind(createUserController));
 userRouter.get('/:id', getByIdUser.run.bind(getByIdUser))
+
+export default userRouter
